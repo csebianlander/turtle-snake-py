cb5d07f19eddce2dbf7589ea5a554e8fed6aa2ab
csebianlander <csebianlander@gmail.com>
2025-07-23 10:06:09 -0400

    Finish base game

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from turtle import Screen, Turtle
 from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
 import time
 
 SPEED = 0.1
@@ -11,6 +13,8 @@ screen.title("PySnake")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -23,3 +27,22 @@ while not game_over:
     screen.update()
     time.sleep(SPEED)
     snake.move()
+
+    # Detect collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+
+    # Detect collision with wall
+    if abs(snake.head.xcor()) > 280 or abs(snake.head.ycor()) > 280:
+        game_over = True
+        scoreboard.game_over()
+
+    # Detect collision with self
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_over = True
+            scoreboard.game_over()
+
+screen.exitonclick()
